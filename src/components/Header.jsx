78aa3402404e3306717acd564cd08d0e05f96362
奥村光貴 <>
2021-11-11 22:12:01 +0900

    fix portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import CoverImage from '../images/cover-image.jpg'
+import ProfileImage from '../images/profile-image.png'
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+
+export const Header = () => {
+
+  return (
+    //HTMLではなくJSX
+    <header className="main-cover" style={{ backgroundImage: `url(${CoverImage})` }}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素 */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
+            <h1 className="title-text">奥村光貴</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                <a href="https://twitter.com/">
+                  <FaTwitter color="white" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://github.com/">
+                  <FaGithub color="white" size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
